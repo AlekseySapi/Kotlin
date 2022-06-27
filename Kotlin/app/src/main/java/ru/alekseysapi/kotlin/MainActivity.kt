ee399dc886ea02db8bce2126d53869ed274c66db
AlekseySapi <altmind92@gmail.com>
2022-06-27 17:07:13 +0300

    Добавил кнопку и повесил clickListener

--- Kotlin/app/src/main/java/ru/alekseysapi/kotlin/MainActivity.kt
+++ Kotlin/app/src/main/java/ru/alekseysapi/kotlin/MainActivity.kt
@@ -2,10 +2,25 @@ package ru.alekseysapi.kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var button : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button = findViewById(R.id.button)
+        button.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id){
+            R.id.button -> {
+                //Действия при нажатии кнопки
+            }
+        }
     }
 }
